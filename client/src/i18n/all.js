4516236c99b894b7161de7edd37fd0fc3cf7a357
George Orwell <damnyougeorgeorwell@gmail.com>
2019-02-07 22:44:35 +0100

    stuff

---
+++ client/src/i18n/all.js
@@ -0,0 +1,9 @@
+import LocalizedStrings from 'react-localization';
+import frenchStrings from './fr'
+export const localisedStringsTuple = new LocalizedStrings({
+ en:{
+
+   choice:"How to choose the egg"
+ },
+ fr: frenchStrings,
+})
---
+++ client/src/i18n/fr.js
@@ -0,0 +1,6 @@
+export const frenchStrings = {
+   how:"Come vuoi il tuo uovo oggi?",
+   boiledEgg:"Uovo sodo",
+   softBoiledEgg:"Uovo alla coque",
+   choice:"Come scegliere l'uovo"
+ }
